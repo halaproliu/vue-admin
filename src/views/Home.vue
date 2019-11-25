6ef52e585a1dee503c10c8f4bf2665cf4b59b0bb
luffy <luffybryant@gmail.com>
2019-11-25 23:33:34 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>hahah</div>
+  <div class="main">
+    <div class="search-box">
+      <div class="search-input">
+        <span>姓名：</span>
+        <el-input placeholder="请输入文字" v-model="name"></el-input>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      name: ''
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.main {
+  background: #fff;
+  height: 100%;
+  padding: 20px;
+}
+
+.search-box {
+}
+
+.search-input {
+  width: 240px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  span {
+    min-width: 60px;
+  }
+}
 </style>
